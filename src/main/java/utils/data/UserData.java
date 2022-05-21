0cd71378003d251f615b976ebf2e04738dcd2b6f
Juan Arguelles <juan.aguelles@globant.com>
2022-05-21 12:37:18 -0500

    random data generation algorythm is working fine

--- src/main/java/utils/data/UserData.java
+++ src/main/java/utils/data/UserData.java
@@ -2,11 +2,63 @@ package utils.data;
 
 import org.testng.annotations.DataProvider;
 
+import java.util.Random;
+
 public class UserData
 {
+    private static Random random;
+    private static String FIRST_NAME;
+    private static String LAST_NAME;
+    private static String EMAIL;
+
+    private static final String[]FIRST_NAMES;
+    private static final String[]LAST_NAMES;
+    private static final String PASSWORD;
+
+    private static final int EMAIL_BOUND;
+    private static final int FIRST_NAMES_BOUND;
+    private static final int LAST_NAMES_BOUND;
+
+    static {
+        random = new Random();
+        FIRST_NAME = LAST_NAME = EMAIL = "";
+
+        PASSWORD = "12345qwerty";
+        FIRST_NAMES = new String[]{"Juan", "Daniela", "Alena", "Dasha"};
+        LAST_NAMES = new String[]{"Arguelles", "Fonseca", "Fox", "Jankowska"};
+        EMAIL_BOUND = 9;
+        FIRST_NAMES_BOUND = 4;
+        LAST_NAMES_BOUND = 4;
+    }
+
     @DataProvider(name = "user")
     public Object[][]inputData()
     {
+        generateAccount();
+        return new Object[][]{{FIRST_NAME, LAST_NAME, EMAIL, PASSWORD}};
+    }
 
+    private static void generateAccount()
+    {
+        generateFullName();
+        generateEmail();
+    }
+
+    private static void generateFullName()
+    {
+        FIRST_NAME = FIRST_NAMES[random.nextInt(FIRST_NAMES_BOUND)];
+        LAST_NAME = LAST_NAMES[random.nextInt(LAST_NAMES_BOUND)];
+    }
+
+    private static void generateEmail()
+    {
+        StringBuilder emailGenerator = new StringBuilder();
+
+        for(int i = 0; i < EMAIL_BOUND; i++)
+        emailGenerator.append((char)(random.nextInt(26)+'a'));
+
+        emailGenerator.append("@yopmail.com");
+
+        EMAIL = emailGenerator.toString();
     }
 }
--- src/test/java/GeneralInteractionTest.java
+++ src/test/java/GeneralInteractionTest.java
@@ -3,6 +3,7 @@ import org.testng.annotations.Test;
 import pages.inside.WelcomeUserPage;
 import pages.outside.AccessPage;
 import pages.outside.SignUpPage;
+import utils.data.UserData;
 
 public class GeneralInteractionTest extends TestBase
 {
@@ -17,10 +18,14 @@ public class GeneralInteractionTest extends TestBase
         this.accessPage = this.indexPage.goToAccess();
     }
 
-    @Test(description = "create account", priority = 0)
-    public void createAccount()
+    @Test(description = "create account", priority = 0,
+    dataProviderClass = UserData.class, dataProvider = "user")
+    public void createAccount(String firstName, String lastName, String email, String password)
     {
-
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(email);
+        System.out.printf(password);
     }
 
 }
